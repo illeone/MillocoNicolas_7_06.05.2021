f57e30707cab39794342289b584f5599e0b292ac
Nicolas <nicolasmilloco@hotmail.com>
2023-05-12 17:19:40 +0200

    fonctionnalités de recherche et de filtrage depuis la barre de recherche
    fonctions filterResearch et filterRecipe pour rechercher et filtrer les recettes par nom, description, appareil, ustensiles et ingrédients. Déclenchement de la recherche à partir de 3 caractères dans l'input

--- script.js
+++ script.js
@@ -1,23 +1,69 @@
 const loadData = async () => {
-    const response = await fetch("recipes.json");
-    const data = await response.json();
-    return data.recipes;
+  const response = await fetch("recipes.json");
+  const data = await response.json();
+  return data.recipes;
+};
+
+const displayRecipe = (elements) => {
+  const recipes = document.getElementById("listRecipes");
+  recipes.innerHTML = "";
+
+  for (let i = 0; i < elements.length; i++) {
+    let ficheRecipes = new Recipe(elements[i]);
+    recipes.innerHTML += ficheRecipes.RecipeCardHTML();
+  }
+};
+
+const filterResearch = (recipe) => {
+    console.log(recipe);
+  
+    const searchInput = document.getElementById('searchTop');
+    searchInput.addEventListener('keyup', function () {
+      const searchInputValue = searchInput.value.toLowerCase();
+  
+      if (searchInputValue.length < 3) {
+        displayRecipe(recipe);
+      } else {
+        filterRecipe(recipe, searchInputValue);
+      }
+    });
   };
   
-  const displayRecipe = (elements) => {
-    const recipes = document.getElementById("listRecipes");
-    recipes.innerHTML = "";
+
+  const filterRecipe = (recipe, searchInputValue) => {
+
+    const result = [];
   
-    for (let i = 0; i < elements.length; i++) {
-      let ficheRecipes = new Recipe(elements[i]);
-      recipes.innerHTML += ficheRecipes.RecipeCardHTML();
+    for (let i = 0; i < recipe.length; i++) {
+      const item = recipe[i];
+  
+      let isSearchInputValueIncluded = true;
+  
+      if (searchInputValue) {
+        isSearchInputValueIncluded = (
+          item.description.toLowerCase().includes(searchInputValue)
+          || item.name.toLowerCase().includes(searchInputValue)
+          || item.appliance.toLowerCase().includes(searchInputValue)
+          || item.ustensils.some(element => element.toLowerCase().includes(searchInputValue))
+          || item.ingredients.some(element => element.ingredient.toLowerCase().includes(searchInputValue))
+        );
+      }
+  
+      if (isSearchInputValueIncluded) {
+        result.push(item);
+      }
     }
-  };
   
-  const init = async () => {
-    const recipe = await loadData();
-    displayRecipe(recipe);
-  };
+    displayRecipe(result);
   
-  init();
+    return result;
+  }
   
+
+const init = async () => {
+  const recipe = await loadData();
+  displayRecipe(recipe);
+  filterResearch(recipe);
+};
+
+init();
